a5b1c0eb9871bd179d21cbec77de392dcec9f283
yoni <yonihod199@gmail.com>
2024-08-21 16:07:40 +0300

    minor change to width

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -56,7 +56,7 @@ export default function Header({
           }}
           classNames={{
             control: (state) =>
-              `border rounded-sm py-2 w-48 h-12 border-[#d2d2d2] ${
+              `border rounded-sm py-2 min-w-48 h-12 border-[#d2d2d2] ${
                 state.isFocused ? "border-[#2684ff]" : ""
               }`,
           }}
